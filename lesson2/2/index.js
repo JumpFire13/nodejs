2132e404978fd5733fed32951681e6c4b50b1b65
JumpFire13 <jumpfire@gmail.com>
2016-06-04 00:06:15 +0500

    lesson2.2

--- lesson2/2/index.js
+++ lesson2/2/index.js
@@ -1,33 +1,43 @@
-var readline = require('readline');
-var luck = require('underscore');
 var fs = require('fs');
+require('sugar');
 var arg = process.argv;
 var file = arg[2];
 
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-console.log('*** Игра "Орел или решка" ***');
-
-rl.question('Орел или решка? Введите 1 или 2:\n', function(answer) {
-    var arr = {1:'Орел', 2:'Решка'};
-    var monet = luck.random(1, 2);
-    if (monet == answer) {
-        console.log(arr[monet]+'. Вы угадали!');
-        if (file !== undefined) {
-            fs.appendFile(file, arr[monet]+'. Вы угадали!\n', function (err) {
-                if (err) throw err;
-            });
+if (file !== undefined) {
+    fs.readFile(file, function (err, data) {
+        if (err) throw err;
+        var text = data.toString().lines(),
+            total = text.length,
+            win = data.toString().each('Вы угадали').length,
+            loss = data.toString().each('Вы не угадали').length;
+        function maxWin() {
+            var max = 0,
+                max1 = 0;
+            for (var i=0; i < total; i++) {
+                if (text[i].has('Вы угадали')) {
+                    max1++;
+                    max = max1 > max ? max1 : max;
+                } else {max1=0}
+            }
+            return max;
         }
-    } else {
-        console.log(arr[monet]+'. Вы не угадали.');
-        if (file !== undefined) {
-            fs.appendFile(file, arr[monet]+'. Вы не угадали.\n', function (err) {
-                if (err) throw err;
-            });
+        function maxLoss() {
+            var max = 0,
+                max1 = 0;
+            for (var i=0; i < total; i++) {
+                if (text[i].has('Вы не угадали')) {
+                    max1++;
+                    max = max1 > max ? max1 : max;
+                } else {max1=0}
+            }
+            return max;
         }
-    }
-    rl.close();
-});
+        console.log("Общее количество партий:", total);
+        console.log("Выигранных партий:", win);
+        console.log("Проигранных партий:", loss);
+        console.log("Соотношение выигранных/проигранных партий:", win+"/"+loss );
+        console.log("Максимальное число побед подряд:", maxWin());
+        console.log("Максимальное число проигрышей подряд:", maxLoss());
+    });
+} else console.log("Не указан файл для анализа");
+
