792e6ac7014d717bfcf39066b602ad518128675d
JumpFire13 <jumpfire@gmail.com>
2016-06-04 00:05:55 +0500

    lesson2.1

--- lesson2/1/index.js
+++ lesson2/1/index.js
@@ -1,5 +1,5 @@
 var readline = require('readline');
-var random = require('random-js')();
+var luck = require('underscore');
 var fs = require('fs');
 var arg = process.argv;
 var file = arg[2];
@@ -11,17 +11,22 @@ var rl = readline.createInterface({
 
 console.log('*** Игра "Орел или решка" ***');
 
-rl.question('Орел или решка? 1 или 2\n', function(answer) {
-    var monet = random.integer(1, 2);
+rl.question('Орел или решка? Введите 1 или 2:\n', function(answer) {
+    var arr = {1:'Орел', 2:'Решка'};
+    var monet = luck.random(1, 2);
     if (monet == answer) {
-        console.log('Вы угадали!');
+        console.log(arr[monet]+'. Вы угадали!');
         if (file !== undefined) {
-            fs.appendFile(file, 'Вы угадали!\n');
+            fs.appendFile(file, arr[monet]+'. Вы угадали!\n', function (err) {
+                if (err) throw err;
+            });
         }
     } else {
-        console.log('Вы не угадали.');
+        console.log(arr[monet]+'. Вы не угадали.');
         if (file !== undefined) {
-            fs.appendFile(file, 'Вы не угадали!\n');
+            fs.appendFile(file, arr[monet]+'. Вы не угадали.\n', function (err) {
+                if (err) throw err;
+            });
         }
     }
     rl.close();
